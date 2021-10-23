efa38210260e59af19de5150b83742db784544e7
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-10-23 19:21:00 +0200

    Use hidden friend idiom for default comparison operator in Position

--- metacore/include/metacore/Position.h
+++ metacore/include/metacore/Position.h
@@ -6,7 +6,7 @@ struct Position final {
     int x = 0;
     int y = 0;
 
-    bool operator==(Position const &) const = default;
+    friend bool operator==(Position const &, Position const &) = default;
 };
 
 } // namespace metacore
