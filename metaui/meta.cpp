4bdd27b8fc42f28ce1ab7312246a7978f9025d9c
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-10-23 19:35:14 +0200

    Tune clang-format and implement rendering of upgrades

--- metaui/meta.cpp
+++ metaui/meta.cpp
@@ -18,14 +18,39 @@ struct ScreenPosition final {
 };
 
 ScreenPosition
-world_position_to_screen_position(metacore::Position const &world_position)
+world_position_to_screen_position(metacore::Position const& world_position)
 {
     auto const x = world_position.x + width / 2;
     auto const y = world_position.y + height / 2;
     return {x, y};
 }
 
-void render_gamestate(SDL_Renderer &renderer, metacore::GameState const &state)
+template <Uint8 red, Uint8 green, Uint8 blue>
+void render_at_position(
+    SDL_Renderer& renderer, metacore::Position const& world_position)
+{
+    auto const screen_position =
+        world_position_to_screen_position(world_position);
+    auto r = SDL_Rect{screen_position.x, screen_position.y, 50, 50};
+    if (SDL_SetRenderDrawColor(&renderer, red, green, blue, 255) != 0) {
+        throw_sdl_error();
+    }
+    if (SDL_RenderFillRect(&renderer, &r) != 0) {
+        throw_sdl_error();
+    }
+}
+
+void render_player(SDL_Renderer& renderer, metacore::Position const& position)
+{
+    render_at_position<0, 0, 255>(renderer, position);
+}
+
+void render_upgrade(SDL_Renderer& renderer, metacore::Position const& position)
+{
+    render_at_position<255, 255, 255>(renderer, position);
+}
+
+void render_gamestate(SDL_Renderer& renderer, metacore::GameState const& state)
 {
     if (SDL_SetRenderDrawColor(&renderer, 255, 0, 0, 255) != 0) {
         throw_sdl_error();
@@ -33,21 +58,14 @@ void render_gamestate(SDL_Renderer &renderer, metacore::GameState const &state)
     if (SDL_RenderClear(&renderer) != 0) {
         throw_sdl_error();
     }
-    auto const player =
-        world_position_to_screen_position(state.player_position);
-    auto r = SDL_Rect{player.x, player.y, 50, 50};
-    if (SDL_SetRenderDrawColor(&renderer, 0, 0, 255, 255) != 0) {
-        throw_sdl_error();
-    }
-    if (SDL_RenderFillRect(&renderer, &r) != 0) {
-        throw_sdl_error();
-    }
+    render_player(renderer, state.player_position);
+    render_upgrade(renderer, state.upgrade);
     SDL_RenderPresent(&renderer);
 }
 
 enum class EscapePressed : bool { Yes, No };
 
-EscapePressed read_and_pass_input(metacore::MetaEngine &engine)
+EscapePressed read_and_pass_input(metacore::MetaEngine& engine)
 {
     auto esc_pressed = EscapePressed::No;
     auto event = SDL_Event{};
@@ -68,9 +86,9 @@ EscapePressed read_and_pass_input(metacore::MetaEngine &engine)
 
 } // namespace
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
-    SDL_Window *window = nullptr;
+    SDL_Window* window = nullptr;
     try {
         if (SDL_Init(SDL_INIT_VIDEO) != 0) {
             throw_sdl_error();
@@ -85,7 +103,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
         if (window == nullptr) {
             throw_sdl_error();
         }
-        auto *const renderer =
+        auto* const renderer =
             SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
         if (renderer == nullptr) {
             throw_sdl_error();
@@ -98,7 +116,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
             }
             SDL_Delay(50);
         }
-    } catch (std::exception const &error) {
+    } catch (std::exception const& error) {
         std::cerr << error.what() << '\n';
         SDL_DestroyWindow(window);
         SDL_Quit();
