53c66fbec19c9ce4efed110ce6ac8b6b01cbaa52
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-11-07 13:39:36 +0100

    Fix tests in EnemiesTest

--- metacore/test/EnemiesTest.cpp
+++ metacore/test/EnemiesTest.cpp
@@ -9,9 +9,15 @@ TEST(EnemiesTest, AdvanceMovesEnemies)
 {
     auto const player_position = PositionD{50, 50};
     auto enemies = Enemies{{{0, 0}, {200, 500}}};
-    enemies.advance(player_position);
-    auto const expected = std::vector<PositionD>{{0, 10}, {200, 490}};
-    EXPECT_EQ(expected, enemies.positions());
+    enemies.advance(player_position, std::chrono::milliseconds{1000});
+    auto const& positions = enemies.positions();
+    EXPECT_EQ(2, positions.size());
+    auto const& enemy1 = positions[0];
+    EXPECT_EQ(0, enemy1.x);
+    EXPECT_GT(enemy1.y, 0);
+    auto const& enemy2 = positions[1];
+    EXPECT_EQ(200, enemy2.x);
+    EXPECT_LT(enemy2.y, 500);
 }
 
 TEST(EnemiesTest, AutomaticallySpawnAdditionalEnemies)
@@ -20,7 +26,7 @@ TEST(EnemiesTest, AutomaticallySpawnAdditionalEnemies)
     for (auto i = 0; i < 4; ++i) {
         enemies.advance(PositionD{0, 0});
     }
-    EXPECT_EQ(1, enemies.positions().size());
+    EXPECT_GT(enemies.positions().size(), 0);
 }
 
 } // namespace
